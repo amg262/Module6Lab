845f81f698a2dfa5796b66abd7551b246f4d6f63
Andrew <andrewmgunn26@gmail.com>
2021-09-25 21:39:04 -0500

    new MD file

--- Program.cs
+++ Program.cs
@@ -5,61 +5,6 @@ namespace Module6Lab
 {
     class Program
     {
-        public void EnterRecipe2()
-        {
-            List<Ingredient> ingredients = new List<Ingredient>();
-            List<Recipe> recipes = new List<Recipe>();
-            string recipeName = "";
-            string ingredientName = "";
-
-            do
-            {
-                Console.WriteLine("Enter Recipe Name");
-                recipeName = Console.ReadLine();
-
-                if (recipeName.ToUpper() != "NA")
-                {
-                    Recipe recipe = new Recipe();
-                    recipe.Name = recipeName;
-                    
-                    do
-                    {
-                        Console.WriteLine("Ingredient (NA when done)>");
-                        ingredientName = Console.ReadLine();
-
-                        if (ingredientName.ToUpper() != "NA")
-                        {
-                            Ingredient ingredient = new Ingredient();
-
-                            Console.WriteLine("Quantity:");
-                            Double.TryParse(Console.ReadLine(), out double quan);
-                            Console.WriteLine("Measurement");
-                            string measure = Console.ReadLine();
-
-                            ingredients.Add(new Ingredient(quan, measure, ingredientName));
-                        }
-
-                        recipe.Ingredients = ingredients;
-                    } while (ingredientName.ToUpper() != "NA");
-                    recipes.Add(recipe);
-                }
-                
-            } while (recipeName.ToUpper() != "NA");
-
-
-            foreach (var recipe in recipes)
-            {
-                Console.WriteLine(recipe.Name);
-
-                foreach (var i in recipe.Ingredients)
-                {
-                    var ingredient = new Ingredient();
-                    Console.WriteLine(ingredient.Print());
-                }
-            }
-            
-        }
-        
         public void EnterRecipe()
         {
             List<Ingredient> ingredients = new List<Ingredient>();
@@ -76,7 +21,7 @@ namespace Module6Lab
                 {
                     Recipe recipe = new Recipe();
                     recipe.Name = recipeName;
-                    
+
                     do
                     {
                         Console.WriteLine("Ingredient (NA when done)>");
@@ -96,58 +41,26 @@ namespace Module6Lab
 
                         recipe.Ingredients = ingredients;
                     } while (ingredientName.ToUpper() != "NA");
+
                     recipes.Add(recipe);
                 }
-                
             } while (recipeName.ToUpper() != "NA");
 
-            
-            
             foreach (Recipe recipe in recipes)
             {
                 Console.WriteLine(recipe.Name);
-            
+
                 foreach (Ingredient i in recipe.Ingredients)
                 {
                     Console.WriteLine(i.Print());
                 }
             }
-            
         }
+
         static void Main(string[] args)
         {
-
             Program p = new Program();
             p.EnterRecipe();
-
         }
-
-        // static void Main2(string[] args)
-        // {
-        //
-        //     List<Ingredient> ings = new List<Ingredient>();
-        //
-        //     string desc;
-        //     do
-        //     {
-        //         Console.WriteLine("Ingrdient (NA when done)");
-        //         desc = Console.ReadLine();
-        //
-        //         if (desc.ToUpper() != "NA")
-        //         {
-        //             Console.WriteLine("Qty: ");
-        //             Int32.TryParse(Console.ReadLine(), out int qty);
-        //             Console.WriteLine("Measure: ");
-        //             string meas = Console.ReadLine();
-        //
-        //             ings.Add(new Ingredient(qty, meas, desc));
-        //         }
-        //     } while (desc.ToUpper() != "NA");
-        //
-        //     foreach (var i in ings)
-        //     {
-        //         Console.WriteLine($"{i.Print()}");
-        //     }
-        // }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-Module6Lab
+# Module 6 Lab #
+
+Modify the Try It program to also have a Recipe class. A Recipe has a List of Ingredients and a name which is a string.
+
+The user should be able to enter multiple recipes. When the user is done, the program should print out all recipes.
+
+```
+Enter recipe name: PB&J Sandwich
+Ingredient (NA when done): Peanut Butter
+Quantity: 3
+Measurement: Tablespoons
+Ingredient (NA when done): Grape Jelly
+Quantity: 2
+Measurement: Tablespoons
+Ingredient (NA when done): Bread
+Quantity: 2
+Measurement: Slices
+Ingredient (NA when done): NA
+Enter another recipe? (Y/N): N
+
+PB&J Sandwich
+3 Tablespoons Peanut Butter
+2 Tablespoons Grape Jelly
+2 Slices Bread
+```
+
+# THIS IS THE FINAL CODE #
+
+```c#
+class Program
+{
+    public void EnterRecipe()
+    {
+        List<Ingredient> ingredients = new List<Ingredient>();
+        List<Recipe> recipes = new List<Recipe>();
+        string recipeName = "";
+        string ingredientName = "";
+
+        do
+        {
+            Console.WriteLine("Enter Recipe Name");
+            recipeName = Console.ReadLine();
+
+            if (recipeName.ToUpper() != "NA")
+            {
+                Recipe recipe = new Recipe();
+                recipe.Name = recipeName;
+
+                do
+                {
+                    Console.WriteLine("Ingredient (NA when done)>");
+                    ingredientName = Console.ReadLine();
+
+                    if (ingredientName.ToUpper() != "NA")
+                    {
+                        Ingredient ingredient = new Ingredient();
+
+                        Console.WriteLine("Quantity:");
+                        Double.TryParse(Console.ReadLine(), out double quan);
+                        Console.WriteLine("Measurement");
+                        string measure = Console.ReadLine();
+
+                        ingredients.Add(new Ingredient(quan, measure, ingredientName));
+                    }
+
+                    recipe.Ingredients = ingredients;
+                } while (ingredientName.ToUpper() != "NA");
+
+                recipes.Add(recipe);
+            }
+        } while (recipeName.ToUpper() != "NA");
+
+        foreach (Recipe recipe in recipes)
+        {
+            Console.WriteLine(recipe.Name);
+
+            foreach (Ingredient i in recipe.Ingredients)
+            {
+                Console.WriteLine(i.Print());
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.EnterRecipe();
+    }
+}
+```
