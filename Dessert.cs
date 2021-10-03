0d9cd75dde449031976438dd8414821117f21b7d
Andrew <andrewmgunn26@gmail.com>
2021-10-03 14:53:13 -0500

    final changes for submission

--- Dessert.cs
+++ Dessert.cs
@@ -1,9 +1,15 @@
-﻿namespace Module6Lab
+﻿using System.Collections.Generic;
+
+namespace Module6Lab
 {
     public class Dessert : Recipe
     {
-        
         public string IsFrozen { get; set; }
-        
+
+        public Dessert(string isFrozen, string name = null, List<Ingredient> ingredients = null) : base(name,
+            ingredients)
+        {
+            IsFrozen = isFrozen;
+        }
     }
 }
--- Entree.cs
+++ Entree.cs
@@ -1,7 +1,15 @@
-﻿namespace Module6Lab
+﻿using System.Collections.Generic;
+
+namespace Module6Lab
 {
     public class Entree : Recipe
     {
         public string Nationality { get; set; }
+
+        public Entree(string nationality, string name = null, List<Ingredient> ingredients = null) : base(name,
+            ingredients)
+        {
+            Nationality = nationality;
+        }
     }
 }
